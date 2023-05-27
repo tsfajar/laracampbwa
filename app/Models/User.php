02bc9fab2e351bdc78d3764ac01358538f051aee
tsfajar <titisfn123@gmail.com>
2023-05-27 17:24:14 +0700

    init commit and user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\softDeletes;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, softDeletes;
