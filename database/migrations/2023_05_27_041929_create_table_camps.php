3f258463c5a97033d2b782317c8cd237584705ab
tsfajar <titisfn123@gmail.com>
2023-05-27 11:36:11 +0700

    init commit and user migration

--- database/migrations/2023_05_27_041929_create_table_camps.php
+++ database/migrations/2023_05_27_041929_create_table_camps.php
@@ -14,7 +14,7 @@ class CreateTableCamps extends Migration
     public function up()
     {
         Schema::table('camps', function (Blueprint $table) {
-            //
+            
         });
     }
 
