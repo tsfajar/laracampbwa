ef6e72cb1e918a7cd04546dcb59071f7438f84e0
tsfajar <titisfn123@gmail.com>
2023-05-27 17:20:10 +0700

    init commit and user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,6 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
-            
         });
     }
 
